2a51d421063713e0e870caba961452eb3038143d
Melody Soriano <mel.soriano6123@gmail.com>
2020-12-08 22:39:04 -0500

    Initial commit

---
+++ FirstConsoleApp/FirstConsoleApp/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace FirstConsoleApp
+{
+    /// <summary>
+    /// First Console App Lecture code
+    /// </summary>
+    class MainClass
+    {
+        /// <summary>
+        /// Prints a message
+        /// </summary>
+        /// <param name="args">command-line arguments</param>
+        public static void Main(string[] args)
+        {
+            //print supportive message
+
+            Console.WriteLine("Hi, n00b!");
+            Console.WriteLine();
+        }
+    }
+}
