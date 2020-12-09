bbf6d3ab8d9806a40516f2dcc5a8e4dadec58567
Melody Soriano <mel.soriano6123@gmail.com>
2020-12-09 13:43:13 -0500

    Exercise2

---
+++ Exercise2/Assets/Scripts/PrintFavoriteGames.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Prints a list of three favorite games
+/// </summary>
+public class PrintFavoriteGames : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        print("My Three Favorite Games: ");
+        print("1. FFXIV");
+        print("2. Overwatch");
+        print("3. The Sims 4");
+    }
+}
---
+++ FirstUnityScript/Assets/Scripts/PrintMessage.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+/// <summary>
+/// Prints a Message
+/// </summary>
+public class PrintMessage : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //Print a supportive message
+        print("Hi, n00b!");
+    }
+}
