d63f2b0a310af6022e20aa1dc3948f58c7354fd6
Melody Soriano <mel.soriano6123@gmail.com>
2020-12-08 22:57:12 -0500

    Exercise 1

---
+++ Exercise1/Exercise1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Exercise1
+{
+    class Program
+    {
+        /// <summary>
+        /// program outputs names
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            //Problem 1
+            //My name!
+            Console.WriteLine("My name is Mel");
+
+            //Problem 2
+            //My friends name!
+            Console.WriteLine("My best friends name is Sarah");
+        }
+    }
+}
