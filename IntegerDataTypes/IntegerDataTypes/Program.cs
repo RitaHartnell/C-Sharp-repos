4d3f9e57df7a0cbcec542fa0a129416dc8b55fec
Melody Soriano <mel.soriano6123@gmail.com>
2020-12-13 17:04:10 -0500

    IntegerDataTypes

---
+++ IntegerDataTypes/IntegerDataTypes/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace IntegerDataTypes
+{
+    /// <summary>
+    /// integer data types lecture code
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// demonstrates integer data types
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            int totalSecondsPlayed = 100;
+            const int SecondsPerMinute = 60;
+
+            // calculate minutes and seconds played
+            int minutesPlayed = totalSecondsPlayed / SecondsPerMinute;
+            int secondsPlayed = totalSecondsPlayed % SecondsPerMinute;
+
+            //print reselts
+            Console.WriteLine("Minutes Played: " + minutesPlayed);
+            Console.WriteLine("Seconds Played: " + secondsPlayed);
+
+            Console.WriteLine();
+        }
+    }
+}
