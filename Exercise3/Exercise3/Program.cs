02814ca6ec248346c813e24ce2a90637d57c29a4
Melody Soriano <mel.soriano6123@gmail.com>
2020-12-13 17:45:58 -0500

    fixed

---
+++ Exercise3/Exercise3/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Exercise3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age = 26;
+
+            Console.WriteLine("My age is: " + age);
+        }
+    }
+}
