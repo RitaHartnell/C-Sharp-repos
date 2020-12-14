ccd94ff55e31cdc488ca70affc3849038d61e4c6
Melody Soriano <mel.soriano6123@gmail.com>
2020-12-14 07:21:14 -0500

    Exercise5

---
+++ Exercise5/Assets/Scripts/PrintPercent.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrintPercent : MonoBehaviour
+{
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        const int MaxScore = 100;
+        int score = 25;
+        float percent = ((float)score / MaxScore) * 100;
+
+        print("Percent of score: " + percent + "%");
+    }
+
+}
