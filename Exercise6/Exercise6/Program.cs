33de44425de33b5891a4f9ce49ddca5d4d324561
Melody Soriano <mel.soriano6123@gmail.com>
2020-12-14 09:00:44 -0500

    Exercise6

---
+++ Exercise6/Exercise6/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Exercise6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please provide an angle in degrees: ");
+            float degAngle = float.Parse(Console.ReadLine());
+            float radAngle = degAngle * (float)Math.PI / 180;
+
+            float cosine = (float)Math.Cos(radAngle);
+            float sine = (float)Math.Sin(radAngle);
+
+            Console.WriteLine("The Cosine is: " + cosine + " " + "The Sine is: " + sine);
+
+        }
+    }
+}
