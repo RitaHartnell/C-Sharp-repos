fc21276af8f16e1c0da57f7d90f0a23f0209697e
Melody Soriano <mel.soriano6123@gmail.com>
2020-12-14 07:14:37 -0500

    Floating Points

---
+++ FloatingPointDataTypes/FloatingPointDataTypes/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace FloatingPointDataTypes
+{
+    /// <summary>
+    /// Floating Point Data Types Lecture code
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// Demonstrates floating point data types
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            int score = 1360;
+            int totalSecondsPlayed = 10000;
+
+            //calculate and print points per second
+            float pointsPerSecond = (float)score / totalSecondsPlayed;
+            Console.WriteLine("Points Per Second: " + pointsPerSecond);
+
+            Console.WriteLine();
+        }
+    }
+}
