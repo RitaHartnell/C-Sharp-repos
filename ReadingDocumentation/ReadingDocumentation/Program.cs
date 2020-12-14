cff2c1fa314fb4bc5f739456ddd8a7b703235ed9
Melody Soriano <mel.soriano6123@gmail.com>
2020-12-14 07:32:34 -0500

    Reading Docs

---
+++ ReadingDocumentation/ReadingDocumentation/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ReadingDocumentation
+{
+    /// <summary>
+    /// reading documentation lecture code
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// Demonstrates reading documentation
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            // calculate and print cosine of 45 degrees
+            float cosine = (float)Math.Cos(45 * Math.PI / 180);
+            Console.WriteLine("Cosine of 45 Degrees: " + cosine);
+
+            Console.WriteLine();
+        }
+    }
+}
